260fb56aea74df3636426ea9a27ed1a6dd9bd6ea
synndicate <dsynn1992@gmail.com>
2025-06-23 12:14:31 -0700

    refactor: rename main page to HomeView/HomeView.swift

--- twitch-ios-client-clone/Features/Home/HomeView.swift
+++ twitch-ios-client-clone/Features/Home/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  twitch-ios-client-clone
 //
 //  Created by Daniel Synn on 6/23/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }

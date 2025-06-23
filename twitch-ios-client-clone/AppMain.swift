e32b711f7ae262048a996418cd061707dadb578e
synndicate <dsynn1992@gmail.com>
2025-06-23 11:45:20 -0700

    refactor: rename app entry point to AppMain.swift

--- twitch-ios-client-clone/AppMain.swift
+++ twitch-ios-client-clone/AppMain.swift
@@ -1,5 +1,5 @@
 //
-//  twitch_ios_client_cloneApp.swift
+//  AppMain.swift
 //  twitch-ios-client-clone
 //
 //  Created by Daniel Synn on 6/23/25.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct twitch_ios_client_cloneApp: App {
+struct AppMain: App {
     var body: some Scene {
         WindowGroup {
             ContentView()

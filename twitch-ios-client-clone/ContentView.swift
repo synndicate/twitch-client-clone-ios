92286733e96e2c812ae72760711b9a9f96dd6b5a
synndicate <dsynn1992@gmail.com>
2025-06-23 11:23:50 -0700

    Initial Commit

---
+++ twitch-ios-client-clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  twitch-ios-client-clone
+//
+//  Created by Daniel Synn on 6/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitch-ios-client-clone/twitch_ios_client_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitch_ios_client_cloneApp.swift
+//  twitch-ios-client-clone
+//
+//  Created by Daniel Synn on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct twitch_ios_client_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
